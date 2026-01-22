dc6da59797fd875e32df865599021d6c2d545571
Ralf Wu <Ralf.Wu@veristat.com>
2026-01-22 17:44:32 +0800

    try

--- R/merge_sdtm.R
+++ R/merge_sdtm.R
@@ -1,22 +1,3 @@
-# list.packages = 
-#   c('data.table', 'dplyr', 'rio')
-# 
-# # Check and install the missing packages
-# new.packages =
-#   list.packages[! list.packages %in% installed.packages()[, "Package"]]
-# 
-# if(length(new.packages)) {
-#   install.packages(new.packages)
-# }
-# # Check and load the missing packages
-# load.packages =
-#   list.packages[! list.packages %in% .packages()]
-# 
-# if(length(load.packages)) {
-#   invisible(lapply(load.packages, library, character.only = TRUE))
-# }
-
-
 # Function for SDTM merging ----
 #' Merge SDTM Main and SUPP Domains
 #'
@@ -27,15 +8,15 @@
 #' @export
 #'
 
-merge.sdtm =
+rw.merge.sdtm =
   function(sdtm, supp){
     
-    sdtm = as.data.table(sdtm)
-    supp = as.data.table(supp)
+    sdtm = data.table::as.data.table(sdtm)
+    supp = data.table::as.data.table(supp)
     
     supp.wide =
       supp[, .(USUBJID, IDVARVAL = as.numeric(IDVARVAL), QNAM, QVAL)]  %>% 
-      arrange(QNAM) %>% 
+      dplyr::arrange(QNAM) %>% 
       reshape(direction = 'wide', idvar = c('USUBJID', 'IDVARVAL'), timevar = 'QNAM',
               v.names = 'QVAL',
               varying = list(supp[order(QNAM), unique(QNAM)]))
