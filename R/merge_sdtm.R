ece3d7db205490b0289ce58d4bf4df027fa0c1c2
Ralf Wu <Ralf.Wu@veristat.com>
2026-01-22 17:12:26 +0800

    try

--- R/merge_sdtm.R
+++ R/merge_sdtm.R
@@ -1,23 +1,32 @@
-list.packages = 
-  c('data.table', 'dplyr', 'rio')
-
-# Check and install the missing packages
-new.packages =
-  list.packages[! list.packages %in% installed.packages()[, "Package"]]
-
-if(length(new.packages)) {
-  install.packages(new.packages)
-}
-# Check and load the missing packages
-load.packages =
-  list.packages[! list.packages %in% .packages()]
-
-if(length(load.packages)) {
-  invisible(lapply(load.packages, library, character.only = TRUE))
-}
+# list.packages = 
+#   c('data.table', 'dplyr', 'rio')
+# 
+# # Check and install the missing packages
+# new.packages =
+#   list.packages[! list.packages %in% installed.packages()[, "Package"]]
+# 
+# if(length(new.packages)) {
+#   install.packages(new.packages)
+# }
+# # Check and load the missing packages
+# load.packages =
+#   list.packages[! list.packages %in% .packages()]
+# 
+# if(length(load.packages)) {
+#   invisible(lapply(load.packages, library, character.only = TRUE))
+# }
 
 
 # Function for SDTM merging ----
+#' Merge SDTM Main and SUPP Domains
+#'
+#' @param sdtm A SDTM main domain
+#' @param supp A SDTM SUPP domain
+#'
+#' @return A merged data.table with all variables from the main domain as well as the variables from all QNAM values 
+#' @export
+#'
+
 merge.sdtm =
   function(sdtm, supp){
     
