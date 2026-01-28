5362b765fdb52acff44807e7438152a98039cf50
Ralf Wu <Ralf.Wu@veristat.com>
2026-01-28 15:44:12 +0800

    testing

--- R/merge_sdtm.R
+++ R/merge_sdtm.R
@@ -5,14 +5,13 @@
 #' @param supp A SDTM SUPP domain
 #'
 #' @return A merged data.table with all variables from the main domain as well as the variables from all QNAM values 
-#'  
+#' 
 #' @export
 
 rw.merge.sdtm =
   function(sdtm, supp){
     # Address check note: undefined global functions or variables
     USUBJID = IDVARVAL = QNAM = QVAL = NULL   # No visible binding for global variable
-    '.' = list()
 
     sdtm = data.table::as.data.table(sdtm)
     supp = data.table::as.data.table(supp)
@@ -24,7 +23,7 @@ rw.merge.sdtm =
     
     supp.wide =
       supp[order(QNAM), 
-           .(USUBJID, IDVARVAL = as.numeric(IDVARVAL), QNAM, QVAL)]  %>% 
+           list(USUBJID, IDVARVAL = as.numeric(IDVARVAL), QNAM, QVAL)]  %>% 
       stats::reshape(direction = 'wide', idvar = reshape.idvar, timevar = 'QNAM',
                      v.names = 'QVAL',
                      varying = list(supp[order(QNAM), unique(QNAM)]))
